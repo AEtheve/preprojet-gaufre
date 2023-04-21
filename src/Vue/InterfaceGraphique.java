a9c96449b365e632e8c9a960a50b3071c59de0a5
AEtheve <30652308+AEtheve@users.noreply.github.com>
2023-04-21 10:35:13 +0200

    Update InterfaceGraphique.java

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -27,8 +27,8 @@ public class InterfaceGraphique extends JComponent{
 
 
         // TODO: s'adapter à la résolution de l'écran qui execute
-
-        fenetre.setSize(largeurCase * plateau.getLength(), hauteurCase * plateau.getWidth()+30);
+        fenetre.setSize(largeurCase * plateau.getLargeur(), hauteurCase * plateau.getHauteur()+30);
+        
         fenetre.setVisible(true);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -41,10 +41,12 @@ public class InterfaceGraphique extends JComponent{
 
     public void clic(int x, int y){
         
-        int i = (x - (fenetre.getWidth() - plateau.getLength() * largeurCase) / 2) / largeurCase;
-        int j = (y - (fenetre.getHeight() - plateau.getWidth() * hauteurCase) / 2) / hauteurCase;
+        // int i = (x - (fenetre.getWidth() - plateau.getLength() * largeurCase) / 2) / largeurCase;
+        // int j = (y - (fenetre.getHeight() - plateau.getWidth() * hauteurCase) / 2) / hauteurCase;
+        int i = (x - (fenetre.getWidth() - plateau.getLargeur() * largeurCase) / 2) / largeurCase;
+        int j = (y - (fenetre.getHeight() - plateau.getHauteur() * hauteurCase) / 2) / hauteurCase;
 
-        if (i < 0 || i >= plateau.getLength() || j < 0 || j >= plateau.getWidth()) {
+        if (i < 0 || i >= plateau.getLargeur() || j < 0 || j >= plateau.getHauteur()) {
             return;
         }
 
@@ -68,9 +70,9 @@ public class InterfaceGraphique extends JComponent{
 
         Point center = new Point(largeur / 2, hauteur / 2);
 
-        for (int k = i; k < plateau.getLength(); k++) {
-            for (int l = j; l < plateau.getWidth(); l++) {
-                drawable.clearRect(center.x - (plateau.getLength() * largeurCase) / 2 + k * largeurCase, center.y + 14 - (plateau.getWidth() * hauteurCase) / 2 + l * hauteurCase, largeurCase, hauteurCase);
+        for (int k = i; k < plateau.getLargeur(); k++) {
+            for (int l = j; l < plateau.getHauteur(); l++) {
+                drawable.clearRect(center.x - (plateau.getLargeur() * largeurCase) / 2 + k * largeurCase, center.y + 14 - (plateau.getHauteur() * hauteurCase) / 2 + l * hauteurCase, largeurCase, hauteurCase);
             }
         }
     }
@@ -86,15 +88,15 @@ public class InterfaceGraphique extends JComponent{
 
         Point center = new Point(largeur / 2, hauteur / 2);
 
-        for (int i = 0; i < plateau.getLength(); i++) {
-            for (int j = 0; j < plateau.getWidth(); j++) {
+        for (int i = 0; i < plateau.getLargeur(); i++) {
+            for (int j = 0; j < plateau.getHauteur(); j++) {
                 if (plateau.estGauffre(i, j)) {
-                    drawable.drawImage(imgGaufre, center.x - (plateau.getLength() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getWidth() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
+                    drawable.drawImage(imgGaufre, center.x - (plateau.getLargeur() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getHauteur() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
                 }
 
                 if (plateau.estPoison(i, j)) {
-                    drawable.drawImage(imgGaufre, center.x - (plateau.getLength() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getWidth() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
-                    drawable.drawImage(imgPoison, center.x - (plateau.getLength() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getWidth() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
+                    drawable.drawImage(imgGaufre, center.x - (plateau.getLargeur() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getHauteur() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
+                    drawable.drawImage(imgPoison, center.x - (plateau.getLargeur() * largeurCase) / 2 + i * largeurCase, center.y + 14 - (plateau.getHauteur() * hauteurCase) / 2 + j * hauteurCase, largeurCase, hauteurCase, null);
                 }
             }
         }
