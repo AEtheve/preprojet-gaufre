d6163e837d30221f50470dae350d610a134734f0
AEtheve <30652308+AEtheve@users.noreply.github.com>
2023-04-21 19:57:41 +0200

    menu joueur + factorisation

--- src/Vue/PlateauGraphique.java
+++ src/Vue/PlateauGraphique.java
@@ -9,7 +9,6 @@ import java.awt.*;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
 
-
 public class PlateauGraphique extends JComponent implements Observateur {
     Image gaufre, poison, gaufre_droit, gaufre_bas, gaufre_coin, poison_bas, poison_droit;
     Jeu j;
@@ -31,54 +30,77 @@ public class PlateauGraphique extends JComponent implements Observateur {
     }
 
     private Image lisImage(String nom) {
-		InputStream in = Configuration.ouvre("Images/" + nom + ".png");
-		Configuration.info("Chargement de l'image " + nom);
-		try {
-			return ImageIO.read(in);
-		} catch (Exception e) {
-			Configuration.erreur("Impossible de charger l'image " + nom);
-		}
-		return null;
-	}
+        InputStream in = Configuration.ouvre("Images/" + nom + ".png");
+        Configuration.info("Chargement de l'image " + nom);
+        try {
+            return ImageIO.read(in);
+        } catch (Exception e) {
+            Configuration.erreur("Impossible de charger l'image " + nom);
+        }
+        return null;
+    }
 
     private void tracer(Graphics2D g, Image i, int x, int y, int l, int h) {
-		g.drawImage(i, x, y, l, h, null);
-	}
+        g.drawImage(i, x, y, l, h, null);
+    }
 
-    public void miseAJour(){
+    public void miseAJour() {
         repaint();
     }
 
     public void paintComponent(Graphics g) {
-		Graphics2D drawable = (Graphics2D) g;
+        Graphics2D drawable = (Graphics2D) g;
         Plateau p = j.getPlateau();
 
-		int largeur = getSize().width;
-		int hauteur = getSize().height;
-		largeurCase = largeur / p.getWidth();
-		hauteurCase = hauteur / p.getHeight();
-		largeurCase = Math.min(largeurCase, hauteurCase);
-		hauteurCase = largeurCase;
+        int largeur = getSize().width;
+        int hauteur = getSize().height;
+        largeurCase = largeur / p.getWidth();
+        hauteurCase = hauteur / p.getHeight();
+        largeurCase = Math.min(largeurCase, hauteurCase);
+        hauteurCase = largeurCase;
+
+        dessinerBarre(drawable, p);
+        dessinerCases(drawable, p);
+
+    }
+
+    private void dessinerBarre(Graphics2D drawable, Plateau p) {
+        int largeur = getSize().width;
+        int hauteur = getSize().height;
+        int barreHauteur = 30;
+        int barreY = 0;
+        int barreX = 0;
+        int barreLargeur = largeur;
 
         xFen = (largeur - largeurCase * p.getWidth()) / 2;
-        yFen = (hauteur - hauteurCase * p.getHeight()) / 2;
+        yFen = (hauteur - hauteurCase * p.getHeight()) / 2 + barreHauteur;
 
-        String title = "Gaufre Empoisonnée - Tour de Joueur " + (j.getPlateau().getPlayer() + 1);
-        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
-        frame.setTitle(title);
+        drawable.setColor(Color.GRAY);
+        drawable.fillRect(barreX, barreY, barreLargeur, barreHauteur);
 
+        String title = "Tour du joueur " + (j.getPlateau().getPlayer() + 1);
+        Font font = new Font("Arial", Font.BOLD, 16);
+        drawable.setFont(font);
+        drawable.setColor(Color.WHITE);
+        FontMetrics metrics = drawable.getFontMetrics(font);
+        int titleWidth = metrics.stringWidth(title);
+        int titleX = (largeur - titleWidth) / 2;
+        int titleY = barreHauteur / 2 + metrics.getAscent() / 2;
+        drawable.drawString(title, titleX, titleY);
+    }
 
+    private void dessinerCases(Graphics2D drawable, Plateau p) {
         for (int i = 0; i < p.getWidth(); i++) {
             for (int j = 0; j < p.getHeight(); j++) {
                 int x = xFen + i * largeurCase;
                 int y = yFen + j * hauteurCase;
 
                 if (p.estGaufre(i, j)) {
-                    if(i+1 < p.getWidth() && p.estMange(i + 1, j) && j+1 < p.getHeight() && p.estMange(i, j+1)){
+                    if (i + 1 < p.getWidth() && p.estMange(i + 1, j) && j + 1 < p.getHeight() && p.estMange(i, j + 1)) {
                         tracer(drawable, gaufre_coin, x, y, largeurCase, hauteurCase);
-                    } else if (i+1 < p.getWidth() && p.estMange(i + 1, j)) {
+                    } else if (i + 1 < p.getWidth() && p.estMange(i + 1, j)) {
                         tracer(drawable, gaufre_droit, x, y, largeurCase, hauteurCase);
-                    } else if (j+1 < p.getHeight() && p.estMange(i, j+1)) {
+                    } else if (j + 1 < p.getHeight() && p.estMange(i, j + 1)) {
                         tracer(drawable, gaufre_bas, x, y, largeurCase, hauteurCase);
                     } else {
                         tracer(drawable, gaufre, x, y, largeurCase, hauteurCase);
@@ -86,8 +108,8 @@ public class PlateauGraphique extends JComponent implements Observateur {
                 }
                 if (p.estPoison(i, j)) {
                     tracer(drawable, poison, x, y, largeurCase, hauteurCase);
-                    tracer(drawable, poison_bas, x, y+hauteurCase, largeurCase, hauteurCase);
-                    tracer(drawable, poison_droit, x+largeurCase, y, largeurCase, hauteurCase);
+                    tracer(drawable, poison_bas, x, y + hauteurCase, largeurCase, hauteurCase);
+                    tracer(drawable, poison_droit, x + largeurCase, y, largeurCase, hauteurCase);
                 }
             }
         }
